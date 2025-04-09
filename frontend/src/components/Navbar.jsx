bb3db3871eb933ca49430e1fe9659cbfb449fbe1
Sachin-Sati <satisachin619@gmail.com>
2025-04-09 20:31:43 +0530

    navbar update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <nav className='navbar navbar-expand-lg navbar-dark mt-0 sticky-top'>
             <div className="container-fluid">
                 <a className='navbar-brand' href='/home'>KeyTempo</a>
-                <div className="collapse navbar-collapse">
-                    <div className="navbar-nav ms-auto">
-                        <a className='nav-link' href="/home">Home</a>
-                        <a className='nav-link' href="/login">Login</a>
-                        <a className='nav-link' href="/register">Register</a>
-                    </div>
+                <button
+                className="navbar-toggler"
+                type="button"
+                onClick={handleClick}
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarContent"
+                aria-controls="navbarContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                    {isOpen ? (
+                        <span class="material-symbols-outlined">close</span>
+                    ) : (
+                        <span className="navbar-toggler-icon"></span>
+                    )}
+                    
+                </button>
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className='nav-link' href="/home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="/login">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="/register">Register</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
